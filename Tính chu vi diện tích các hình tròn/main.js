4e97faeb645275fc185a3c7ca124c98a998cc732
phamngocphuonganh22 <114391167+phamngocphuonganh22@users.noreply.github.com>
2022-11-28 15:20:26 +0700

    nop bt

---
+++ Tính chu vi diện tích các hình tròn/main.js
@@ -0,0 +1,12 @@
+var a = null;
+var pi = 3.14;
+var c = null;
+let R= prompt("nhap ban kinh hinh tron")
+function M() {
+    let c= 2*pi*R;
+    document.write('chu vi: ' +C);
+}
+function show() {
+    let S= pi*R^2;
+    document.write('Dien tich: ' +S);
+}
